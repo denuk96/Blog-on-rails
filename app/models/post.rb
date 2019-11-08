011a70dc6fe6b95dfda563c01f2b8a82b55a3a5d
denuk96 <e_den9611@live.com>
2019-11-08 14:28:23 +0200

    bootstrap, validation added

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, :content , presence: true,
+            length: { minimum: 5 }
+  validates :name, presence: true,
+            length: { minimum: 3 }
 end
