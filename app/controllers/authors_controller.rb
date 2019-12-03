ec00948be0098a273db1ccaecf42dbd593247b64
denuk96 <e_den9611@live.com>
2019-12-03 22:29:31 +0200

    Authors_contrl fixed

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,17 +1,8 @@
 class AuthorsController < ApplicationController
-  before_action :set_author, only: %i[show edit update destroy]
-
-  def index
-    @author = Author.all
-  end
-
-  def show; end
 
   def new
     @author = Author.new
   end
-
-  def edit; end
 
   def create
     @author = Author.new(author_params)
@@ -25,29 +16,7 @@ class AuthorsController < ApplicationController
     end
   end
 
-  def update
-    respond_to do |format|
-      if @author.update(author_params)
-        format.html { redirect_to @author, notice: 'User was successfully updated.' }
-      else
-        format.html { render :edit }
-      end
-    end
-  end
-
-  def destroy
-    @author.destroy
-    respond_to do |format|
-      format.html { redirect_to author_url, notice: 'User was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
-
   private
-
-  def set_author
-    @author = Author.find(params[:id])
-  end
 
   def author_params
     params.require(:author).permit(:email, :password, :password_confirmation, :first_name, :last_name)
