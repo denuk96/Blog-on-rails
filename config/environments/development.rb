0ca0666fc7d7f66776184e2da605bcd11874ed15
denuk96 <e_den9611@live.com>
2019-12-10 19:49:30 +0200

    deploying bug fixed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,8 +1,19 @@
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: '3000' }
 
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.delivery_method = :letter_opener
+  #config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: 'example.com',
+      user_name: 'blog.on.rails.test@gmail.com',
+      password: Rails.application.credentials.aws[:gmail],
+      authentication: 'plain',
+      enable_starttls_auto: true
+  }
 
 
 
